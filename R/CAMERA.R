3baa1bab5a0c83b72ec4d4e36933081eb5a868b6
Yoonsu Cho <35306755+universe77@users.noreply.github.com>
2022-06-01 16:24:54 +0100

    Revert "Add importFrom susieR"
    This reverts commit da594dbcb94a7dc25937820b1f4d292a97217914.

--- R/CAMERA.R
+++ R/CAMERA.R
@@ -463,7 +463,6 @@ CAMERA <- R6::R6Class("CAMERA", list(
 #' @param dat Genomic regions identified by using \code{x$extract_instrument_regions()}
 #' @param ld LD matrix obtained by using \code{x$regional_ld_matrices()}
 #' @return Result from susieR in x$susie_results. Data frame in x$instrument_susie
-#' @importFrom susieR susie_rss
  susie_finemap_regions = function(dat=self$instrument_regions, ld=self$ld_matrices)
   {
    resnps <- names(ld)
